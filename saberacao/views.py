0d543fbc7445c2b5b01c191f75ecb8e984404d56
attiquetecnologia <rodrigoatique>
2025-08-13 14:04:20 -0300

    carrosel de livros

--- saberacao/views.py
+++ saberacao/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Livro
 
 def index(request):
-    return render(request, 'index.html')
+    
+    livros = Livro.objects.all().order_by('-id')[:9]
+    livros_agrupados = []
+    for i in range(0, len(livros), 3):
+        livros_agrupados.append(livros[i:i+3])
+
+    context = { 
+        'livros': livros_agrupados,
+    }
+    return render(request, 'index.html', context)
