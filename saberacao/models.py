4f90514407a69b5db406eacd03ce0bd725c528e8
attiquetecnologia <rodrigoatique>
2025-08-13 13:18:10 -0300

    permitindo anexos em atividades

--- saberacao/models.py
+++ saberacao/models.py
@@ -1,5 +1,16 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+import os
 from datetime import date
+
+# Função para validar a extensão do arquivo
+def validate_file_extension(value):
+    ext = os.path.splitext(value.name)[1]  # Obtém a extensão do arquivo
+    valid_extensions = ['.png', '.pdf', '.docx', '.jpg', '.jpeg']
+    if not ext.lower() in valid_extensions:
+        raise ValidationError(_('Tipo de arquivo não suportado. Por favor, envie um arquivo .png, .pdf, .docx, ou .jpg.'))
+
 
 class Livro(models.Model):
     id = models.AutoField(primary_key=True)
@@ -37,13 +48,12 @@ class Atividade(models.Model):
     id = models.AutoField(primary_key=True)
     titulo = models.CharField(max_length=150, null=False, blank=False)
     descricao = models.CharField(max_length=1000, null=False, blank=False)
-    anexo = models.CharField(max_length=2000, null=True, blank=True)
+    anexo = models.FileField(upload_to='atividades/anexos/', validators=[validate_file_extension], null=True, blank=True)
     data_de_criacao = models.DateField(default=date.today, null=False, blank=False)
     data_de_entrega = models.DateField(null=True, blank=True)
     links = models.CharField(max_length=2000, null=True, blank=True)
     nota = models.FloatField(null=True, blank=True)
 
-    # Relacionamentos com Django ORM
     livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
     aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
 
